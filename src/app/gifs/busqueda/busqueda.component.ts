5c1da7bafbfc9d99a44af057df5a96254832a85c
Picoquio <84530694+Picoquio@users.noreply.github.com>
2022-08-20 21:01:30 -0300

    controlando el historial de busquedas

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -13,11 +13,17 @@ export class BusquedaComponent  {
 
   constructor(private gifsService:GifsService) {} //importamos el service
 
-
+  
   
   buscar() {
     
     const valor = this.jeje.nativeElement.value;
+
+    //cortamos la ejecución de la función si se ingresa un valor vacío
+    if (valor.trim().length === 0) {
+      this.jeje.nativeElement.value = ''; //reinicia el input, por prolijidad
+      return 
+    }
 
     this.gifsService.buscarGifs(valor)
     
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,8 +12,20 @@ export class GifsService {
   }
 
   buscarGifs(query:string) {
+
+    query = query.trim().toLowerCase(); // los argumentos recibos serán en minúscula.
+
+    if (this._historial.includes(query)) {
+      return /* si lo ingresado en la caja de texto ya se encuentra en el array, 
+      corta la ejecución de la función */
+    }
+
     this._historial.unshift(query)
-    console.log(this._historial);
+    
+    //asigna a this._historial el valor de los 10 primeros elementos eliminados
+    this._historial = this._historial.splice(0,10)
+    
+    
     
   }
 
